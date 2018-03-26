ff2487c227e9347b4cf3ff5c40d9ab1f8e18fca3
aanggoro <1511501288@student.budiluhur.ac.id>
2018-03-26 15:01:06 +0700

    add method filtered in model User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    render json: @users
+    render json: @users, include: [:reviews], methods: [:filtered_books], exclude: [:password_digest]
   end
 end
