20a4b709f4b46c49dd987f77f43b4e2c6b5b8957
aanggoro <1511501288@student.budiluhur.ac.id>
2018-03-26 23:01:26 +0700

    add except in controller user and added serializer user, book, review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    render json: @users, include: [:reviews], methods: [:filtered_books], exclude: [:password_digest]
+    render json: @users, except: [:password_digest]
   end
 end
